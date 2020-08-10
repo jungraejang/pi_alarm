50ebcfba2a01fa737305174e7cd35d8bcc09ee2d
jungraejang <jungraejang@pursuit.org>
2020-08-10 18:52:46 -0400

    added mp3 pla1y

--- button.py
+++ button.py
@@ -1,14 +1,6 @@
 import RPi.GPIO as GPIO
-
 import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("thevalkyrie.mp3")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
 
-def button_callback(channel):
-    print("Button was pushed")
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -19,3 +11,14 @@ GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 message = input("Press enter to quit\n\n")
 
 GPIO.cleanup()
+
+def button_callback(channel):
+    print()
+    print("Button was pushed")
+
+
+pygame.mixer.init()
+pygame.mixer.music.load("thevalkyrie.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue

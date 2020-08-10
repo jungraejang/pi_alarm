1c16362ce8f73f550ead81bfef4d27dd44757fc4
jungraejang <jungraejang@pursuit.org>
2020-08-10 18:53:24 -0400

    added mp3 pla16y

--- button.py
+++ button.py
@@ -6,15 +6,17 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+def button_callback(channel):
+    print()
+    print("Button was pushed")
+
 GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
 message = input("Press enter to quit\n\n")
 
 GPIO.cleanup()
 
-def button_callback(channel):
-    print()
-    print("Button was pushed")
+
 
 
 pygame.mixer.init()

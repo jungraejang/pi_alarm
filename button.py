f43c14369f719a2a89be72a8bd15bd33fd6f0186
jungraejang <jungraejang@pursuit.org>
2020-08-10 20:30:33 -0400

    added mp3 pla16h3y

--- button.py
+++ button.py
@@ -1,29 +1,30 @@
 import RPi.GPIO as GPIO
 import pygame
+import schedule
+import time
+
+def alarm(t):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    pygame.mixer.init()
+    pygame.mixer.music.load("thevalkyrie.mp3")
+    pygame.mixer.music.play()
 
-\
-pygame.mixer.init()
-pygame.mixer.music.load("thevalkyrie.mp3")
-pygame.mixer.music.play()
+    def button_callback(channel):
+        print("Button was pushed")
+        pygame.mixer.music.stop()
 
-def button_callback(channel):
-    print("Button was pushed")
-    pygame.mixer.music.stop()
+    GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+    message = input("Press enter to quit\n\n")
 
-message = input("Press enter to quit\n\n")
+    GPIO.cleanup()
 
-GPIO.cleanup()
+schedule.every().day.at("20:33").do(alarm,'It is 01:00')
 
-
-
-
-
-# while pygame.mixer.music.get_busy() == True:
-#     continue
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
